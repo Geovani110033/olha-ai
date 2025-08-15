d35a985e5263a1cc83e44a5663f53716fa477676
Geovani110033 <go9123497@gmail.com>
2025-08-14 21:14:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuLinks = document.querySelectorAll('nav ul li a[href^="#"]');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
